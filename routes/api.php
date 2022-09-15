d569b222659904eb19b3a99edd25c40dca63a2ee
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-09-15 20:20:21 -0300

    Criado controllers e algumas rotas

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register'])->name('auth.register'); //registra um novo vendedor
 Route::post('/login', [AuthController::class, 'login'])->name('auth.login'); //loga o vendedor
+
+Route::group(['middleware' => 'auth:api'], function () { //grupo de rotas autenticadas
+    Route::post('/me', [AuthController::class, 'me'])->name('auth.me'); //retorna os dados dos jornalistas
+
+    Route::post('/clientes/create', [Clientes::class, 'store'])->name('clientes.create'); //cria um novo cliente
+    Route::get('/clientes/lista', [Clientes::class, 'list'])->name('clientes.listar'); //lista todos os clientes
+    Route::post('/clientes/delete', [Clientes::class, 'delete'])->name('clientes.delete'); //cria um novo cliente
+});
