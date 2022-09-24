09589a9e33be318726b36eda1cb25d611327a649
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-09-24 19:41:05 -0300

    Implementado Teste de registro de úsuario

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class register extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testRegister()
+    {
+        $response = $this->get('/');
+        $baseUrl= "http://localhost:8000/api/register";
+
+        $nome = "teste ferreira dos santos";
+        $email = "usuario@teste.com.br";
+        $senha = "senha123";
+
+        $response = $this->json('POST', $baseUrl . '/', [
+            'nome'=>$nome,
+            'email' => $email,
+            'password' => $senha
+        ]);
+
+        $response
+        ->assertStatus(200)
+        ->assertJsonStructure([
+            'authorization', 'status', 'user'
+        ]);
+    }
+}
