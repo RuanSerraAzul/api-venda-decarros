edba9ea8e53f0de6bafdd62becadd9b243fc056b
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-09-24 21:22:33 -0300

    Teste de login implementado + criação de testes carros

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -23,7 +23,7 @@ class register extends TestCase
         $senha = "senha123";
 
         $response = $this->json('POST', $baseUrl . '/', [
-            'nome'=>$nome,
+            'nome' => $nome,
             'email' => $email,
             'password' => $senha
         ]);
@@ -33,5 +33,27 @@ class register extends TestCase
         ->assertJsonStructure([
             'authorization', 'status', 'user'
         ]);
+    }
+
+    public function testLogin(){
+        $response = $this->get('/');
+        $baseUrl= "http://localhost:8000/api/login";
+
+        $email = "usuario@teste.com.br";
+        $senha = "senha123";
+
+        $response = $this->json('POST', $baseUrl . '/', [
+            'email' => $email,
+            'password' => $senha
+
+        ]);
+
+        $response
+        ->assertStatus(200)
+        ->assertJsonStructure([
+            'access_token', 'token_type', 'expires_in'
+        ]);
+
+
     }
 }
