87017c8050fd5fc74373e7fed1b4ee11035f39db
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-09-25 22:59:20 -0300

    changed name class

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class register extends TestCase
+class RegisterTest extends TestCase
 {
     /**
      * A basic feature test example.
